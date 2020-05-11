6068fbe7b30c841ca04ee150491f7e2e4a3a11cb
jargondizza <argondizz@hartford.edu>
2020-05-11 01:54:43 -0400

    Add constants for each box letter

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const boxA = document.getElementsByTagName(`td`).item(0);
+const boxB = document.getElementsByTagName(`td`).item(1);
+const boxC = document.getElementsByTagName(`td`).item(2);
+const boxD = document.getElementsByTagName(`td`).item(3);
