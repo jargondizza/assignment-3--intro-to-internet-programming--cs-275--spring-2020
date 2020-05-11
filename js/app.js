dbd24da84b7e138e75fa6c7937471da19f7b6ccb
jargondizza <argondizz@hartford.edu>
2020-05-11 01:55:47 -0400

    Add boxA EventListener function

--- js/app.js
+++ js/app.js
@@ -1,4 +1,21 @@
 const boxA = document.getElementsByTagName(`td`).item(0);
+boxA.addEventListener(`click`, flipA);
 const boxB = document.getElementsByTagName(`td`).item(1);
 const boxC = document.getElementsByTagName(`td`).item(2);
 const boxD = document.getElementsByTagName(`td`).item(3);
+function flipA() {
+    if (boxA.style.transform != `rotate(180deg)`){
+        boxA.style.transform = `rotate(180deg)`;
+        boxA.style.color = `#364aff`;
+    }
+    else unflipA();
+    unflipB();
+    unflipC();
+    unflipD();
+}
+
+function unflipA() {
+    boxA.style.transform = `rotate(0deg)`;
+    boxA.style.color = `#222`;
+}
+
